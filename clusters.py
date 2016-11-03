6e6dbfb589398f246a701936ffcd6597ac2de4ac
dongshujin <dongshujin.beans@gmail.com>
2016-11-03 17:40:01 +0800

    Change: 绘制树状图

--- clusters.py
+++ clusters.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import math
+from PIL import Image, ImageDraw
 
 def readfile(filename) :
     lines = [line for line in file(filename)]
@@ -105,3 +106,64 @@ def printclust(clust, labels=None, n=0) :
         printclust(clust.left, labels=labels, n = n+1)
     if clust.right != None:
         printclust(clust.right, labels=labels, n= n+1)
+
+
+def getheight(clust) :
+    # 叶节点 高度为 1
+    if clust.left == None and clust.right == None:
+        return 1
+
+    # 非叶子节点 高度为 分支节点高度之和
+    return getheight(clust.left) + getheight(clust.right)
+
+def getdepth(clust) :
+    # 叶子节点的距离是 0.0
+    if clust.left == None and clust.right == None:
+        return 0.0
+
+    # 非叶子节点的距离是 两个分支中距离较大者 + 节点本身距离
+    return max(getdepth(clust.left), getdepth(clust.right)) + clust.distance
+
+def drawdendrogram(clust, labels, jpeg="out/cluster.jpg") :
+    # 高度和宽度
+    h = getheight(clust)*20
+    w = 1200
+    depth = getdepth(clust)
+
+    # 宽度固定,所以我们对距离进行相应的缩放
+    scaling = float(w-150)/depth
+
+    # 建立一个白色背景图片
+    img = Image.new('RGB', (w, h), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+
+    draw.line((0, h/2, 10,h/2), (255, 0, 0))
+
+    # 画第一个节点
+    drawnode(draw, clust, 10, (h/2), scaling, labels)
+    img.save(jpeg, 'JPEG')
+
+def drawnode(draw, clust, x, y, scaling, labels) :
+    if clust.id < 0 :
+        h1 = getheight(clust.left)*20
+        h2 = getheight(clust.right)*20
+        top = y-(h1+h2)/2
+        bottom = y+(h1+h2)/2
+
+        ll = clust.distance * scaling
+
+        draw.line((x, top+h1/2, x, bottom-h2/2), fill=(255, 0, 0))
+        draw.line((x, top+h1/2, x+ll, top+h1/2), fill=(255, 0, 0))
+        draw.line((x, bottom-h2/2, x+ll, bottom-h2/2), fill=(255, 0, 0))
+        drawnode(draw, clust.left, x+ll, top+h1/2, scaling, labels)
+        drawnode(draw, clust.right, x+ll, bottom-h2/2, scaling, labels)
+    else:
+        # 叶子节点 绘制节点的标签
+        draw.text((x+5, y-7), labels[clust.id], (0,0,0))
+
+
+
+blognames, words, data = readfile('out/blog.data')
+clust = hcluster(data)
+
+drawdendrogram(clust, blognames)
